1b49d91e43e0e5665791119f3d635c3dd081bf75
Connor Jardine <cjardine@Unknown-7c-dd-90-3b-44-ce.home>
2018-02-28 12:32:38 +0000

    bfaf work so far

--- endpoints/rest/transaction.py
+++ endpoints/rest/transaction.py
@@ -3,10 +3,12 @@ import logging
 from flask import Blueprint, request, current_app, jsonify
 
 from services.transactionservice.client import TransactionServiceClient
+from services.accountsservice.client import AccountsServiceClient
 
 
 logger = logging.getLogger(__name__)
 transaction_blueprint = Blueprint('transaction', __name__)
+account_blueprint = Blueprint('accounts',__name__)
 
 
 @transaction_blueprint.route('/send', methods=['POST'])
@@ -25,11 +27,31 @@ def send_money():
     }), 200
 
 
-@transaction_blueprint.route('/deposit', methods=['POST'])
+@account_blueprint.route('/deposit', methods=['POST'])
 def deposit():
-    raise NotImplementedError()
+    client = AccountsServiceClient(current_app.config.get("ACCOUNT_SERVICE_URL", "localhost"))
+    data = request.get_json()
+
+    client.do_account_deposit(
+        data.get("to_account_id"),
+        data.get("amount")
+    )
+
+    return jsonify({
+        "status": "pending"
+    }), 200
 
 
-@transaction_blueprint.route('/withdrawal', methods=['POST'])
+@account_blueprint.route('/withdrawal', methods=['POST'])
 def withdrawal():
-    raise NotImplementedError()
+    client = AccountsServiceClient(current_app.config.get("ACCOUNT_SERVICE_URL", "localhost"))
+    data = request.get_json()
+
+    client.do_account_withdraw(
+        data.get("from_account_id"),
+        data.get("amount")
+    )
+
+    return jsonify({
+        "status": "pending"
+    }), 200
--- services/accountsservice/client.py
+++ services/accountsservice/client.py
@@ -1,5 +1,10 @@
+import logging
+import json
+
 from services.accountsservice.exception import AccountsServiceException, AccountNotFoundException
 from services.baseclient import ServiceClient
+
+logger = logging.getLogger(__name__)
 
 
 class AccountsServiceClient(ServiceClient):
@@ -22,5 +27,35 @@ class AccountsServiceClient(ServiceClient):
         elif response.status_code == 404:
             raise AccountNotFoundException("Account {} not found!".format(account_id))
         else:
+            raise AccountsServiceException("Accounts Service Error: status={} body='{}'"
+                    .format(response.status_code, response.content))
+            
+            
+    #For both of these methods, how do I deal with the response.status_code? 
+    #I had a look at transactionserive/client.py and there is no code handling
+            
+    def do_account_deposit(self, to_account_id, amount):
+        payload = {
+            'toAccountId': to_account_id,
+            'amount': amount
+        }
+        logger.debug("Sending /AccountDeposit request: {}".format(json.dumps(payload)))
+        
+        response = self._session.post("{}/AccountDeposit".format(self.url), json.dumps(payload))
+
+        if not response.ok:
+            raise AccountsServiceException("Accounts Service Error: status={} body='{}'"
+                    .format(response.status_code, response.content))
+            
+    def do_account_withdraw(self, from_account_id, amount):
+        payload = {
+            'fromAccountId': from_account_id,
+            'amount': amount
+        }
+        logger.debug("Sending /AccountWithdraw request: {}".format(json.dumps(payload)))
+        
+        response = self._session.post("{}/AccountWithdraw".format(self.url), json.dumps(payload))
+
+        if not response.ok:
             raise AccountsServiceException("Accounts Service Error: status={} body='{}'"
                     .format(response.status_code, response.content))
